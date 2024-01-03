d5a651eba9d0dae78e45469fa5c24d2f41a2b6e7
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-01-03 19:20:10 +0100

    feat: :sparkles: adding auto linked issues

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,11 +5,22 @@ const path = require('node:path');
 const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
 const apps = fs.readdirSync(path.resolve(__dirname, 'apps'));
 const libs = fs.readdirSync(path.resolve(__dirname, 'libs'));
+
+const { execSync } = require('child_process');
+
+// @tip: git branch name = feature/issue_33   =>    auto get defaultIssues = #33
+const issue = execSync('git rev-parse --abbrev-ref HEAD')
+  .toString()
+  .trim()
+  .split('_')[1];
+
 module.exports = {
   rules: {
     'scope-enum': [2, 'always', [...packages, ...apps, ...libs]],
   },
   prompt: {
     useEmoji: true,
+    customIssuePrefixAlign: !issue ? 'top' : 'bottom',
+    defaultIssues: !issue ? '' : `#${issue}`,
   },
 };
