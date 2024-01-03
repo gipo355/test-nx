d369d8f36e4b3f2bd181babbddabe69ac2f3dd95
gipo355 <91525512+gipo355@users.noreply.github.com>
2024-01-03 20:06:11 +0100

    remove unused projects

--- libs/sveltekit/src/generators/sveltekit-generator/schema.d.ts
+++
@@ -1,3 +0,0 @@
-export interface SveltekitGeneratorGeneratorSchema {
-    name: string;
-}
--- libs/testlib/src/lib/testlib.spec.ts
+++
@@ -1,7 +0,0 @@
-import { testlib } from './testlib';
-
-describe('testlib', () => {
-  it('should work', () => {
-    expect(testlib()).toEqual('testlib');
-  });
-});
